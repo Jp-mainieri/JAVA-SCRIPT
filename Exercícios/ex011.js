d876579623a97c6f313faee8127d251739b23707
João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
2023-09-02 14:57:20 -0300

    Better organization

---
+++ Exercícios/ex011.js
@@ -0,0 +1,17 @@
+var age = 18
+console.log(`Your age is ${age}`)
+
+if  (age < 16){
+
+    console.log('You cannot vote yet')
+    
+} else if (age < 18 || age > 65) {
+
+    console.log('You can vote, although its not obligatory')
+    
+
+}else {
+
+    console.log('You must vote at your age')
+
+}
